64e98c39a4eb2a86c0a5490971574aa2a6d55ba1
Mrchuchi <mrchuchi@icloud.com>
2025-11-10 08:55:53 -0500

    ...-

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ["frontend-confirmacionasistenacia.up.railway.app"],
+    host: "0.0.0.0",
+    port: 5173
+  }
 })
